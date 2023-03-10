bea6465b8f5452e24bae7ee837cfe66c45fa4664
passchn <77938819+passchn@users.noreply.github.com>
2023-03-10 19:16:38 +0100

    bin/cake bake articles ... after DB table creation and Assets migration

---
+++ src/Model/Entity/Article.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Article Entity
+ *
+ * @property string $id
+ * @property string $title
+ * @property string|null $content
+ * @property string|null $image_id
+ * @property \Cake\I18n\FrozenTime $created
+ * @property \Cake\I18n\FrozenTime $modified
+ */
+class Article extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array<string, bool>
+     */
+    protected $_accessible = [
+        'title' => true,
+        'content' => true,
+        'image_id' => true,
+        'created' => true,
+        'modified' => true,
+    ];
+}
